9921b1cb2a3df439a22fbd45e68a0f1cac0b3606
Airam A. <airam.a.g.85@gmail.com>
2023-10-25 18:21:59 +0100

    Impresión de variables en HTML

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,18 @@
+const newMessage = {
+    message: 'Hello World!',
+    title: 'FirstApp',
+}
+
+const getResult = (a, b) => {
+    return a + b;
+}
+
 // Lo que está englobado por <></> puede hacerse también con Fragment
 export const FirstApp = () => {
     return (
         <>
-            <div>FirstApp</div>
+            <h1>{ getResult(5, 10) }</h1>
+            {/* <h1>{ JSON.stringify( newMessage )}</h1> */}
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, iste? Laboriosam nesciunt molestias saepe placeat ut debitis quos, ullam, soluta pariatur accusantium vero incidunt, iste omnis fugit rerum. Tempore, sapiente.</p>
         </>
     )
