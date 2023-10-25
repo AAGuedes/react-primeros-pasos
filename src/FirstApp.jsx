236c094ec8dbd3788d53f037a4a23d087f4d7dce
Airam A. <airam.a.g.85@gmail.com>
2023-10-25 15:13:35 +0100

    Retornar elementos en el Componente - Fragment

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,5 +1,9 @@
+// Lo que está englobado por <></> puede hacerse también con Fragment
 export const FirstApp = () => {
     return (
-        <div>FirstApp</div>
+        <>
+            <div>FirstApp</div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, iste? Laboriosam nesciunt molestias saepe placeat ut debitis quos, ullam, soluta pariatur accusantium vero incidunt, iste omnis fugit rerum. Tempore, sapiente.</p>
+        </>
     )
 }
