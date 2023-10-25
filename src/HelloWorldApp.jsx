fce6d297589371d4a227ac98fc0a85a6ea42920d
Airam A. <airam.a.g.85@gmail.com>
2023-10-25 14:25:43 +0100

    Nuestro primer componente

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,12 @@
+// function App() {
+//     return <h1>Hello World!</h1>;
+// }
+
+// export default App;
+
+// Creado con rafc
+export const HelloWorldApp = () => {
+  return (
+    <h1>HelloWorldApp</h1>
+  )
+}
