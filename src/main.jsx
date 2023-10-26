4e1f81876cb174c793f95e2790221d0ac113ab47
Airam A. <airam.a.g.85@gmail.com>
2023-10-26 08:06:01 +0100

    Tarea - Componente CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // import { HelloWorldApp } from './HelloWorldApp';
-import { FirstApp } from './FirstApp';
+// import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
 
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp />
+        <CounterApp value={ 0 }/>
     </React.StrictMode>
 );
