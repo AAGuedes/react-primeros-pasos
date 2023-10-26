d88730b49c4fc539c316a4f19b719faf8a7209e0
Airam A. <airam.a.g.85@gmail.com>
2023-10-26 08:24:34 +0100

    Evento click (Eventos en general

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -2,11 +2,20 @@ import PropTypes from "prop-types";
 
 
 
+const handleAdd = () => {
+    console.log('+1')
+}
+
 export const CounterApp = ({ value }) => {
+
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
+            <button onClick={handleAdd}>
+                +1
+            </button>
         </>
     );
 }
