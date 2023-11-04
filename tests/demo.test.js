1d100043aff2013d1279c0c7b543774a85e9bebb
Airam A. <airam.a.g.85@gmail.com>
2023-11-04 22:08:43 +0000

    Jest - Expect - toBe

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,5 +1,16 @@
-test('Esta prueba no debe fallar', () => {
-    if(0 === 0) {
-        throw new Error('No puede dividir entre cero');
-    }
-})
+describe('Pruebas en <DemoComponent />', () => {
+
+    test('Esta prueba no debe fallar', () => {
+
+        // 1. Inicialización
+        const message1 = 'Hola Mundo!';
+
+        // 2. Estímulo
+        const message2 = message1.trim();
+
+        // 3. Observar el comportamiento
+        expect(message1).toBe(message2);
+
+    });
+
+});
