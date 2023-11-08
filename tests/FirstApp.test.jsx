22cfd2f147598b46d9313ed4cb15cb8dd7059ac5
Airam A. <airam.a.g.85@gmail.com>
2023-11-08 22:22:14 +0000

    Probar FirstApp

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -7,8 +7,21 @@ describe('Pruebas de <FirstApp />', () => {
     test('Debe de coincidir con el Snapshot', () => {
 
         const title = `Hello, I'm John Doe`;
+        const { container } = render(<FirstApp title={title} />);
 
-        render(<FirstApp title={title} />);
+        expect(container).toMatchSnapshot();
+
+    });
+
+    test('Debe mostrar el título en un h1', () => {
+
+        const title = `Hello, I'm John Doe`;
+        const { container, getByText } = render(<FirstApp title={title} />);
+
+        expect(getByText(title)).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title);
 
     });
 
