649caad3573b33a0a7b25db56a4bba5653afe3bf
Airam A. <airam.a.g.85@gmail.com>
2023-11-08 19:03:54 +0000

    Pruebas sobre componentes de React

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // import { HelloWorldApp } from './HelloWorldApp';
-// import { FirstApp } from './FirstApp';
-import { CounterApp } from './CounterApp';
+import { FirstApp } from './FirstApp';
+// import { CounterApp } from './CounterApp';
 
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <CounterApp value={ 0 }/>
+        {/* <CounterApp value={ 0 }/> */}
+        <FirstApp />
     </React.StrictMode>
 );
